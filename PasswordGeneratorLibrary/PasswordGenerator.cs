9be9e23fb1a3b67d17cc54b24abcf6f28fa82991
Joris <jcap.wouters@gmail.com>
2016-12-24 21:06:13 +0100

    Initial Commit

---
+++ PasswordGeneratorLibrary/PasswordGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace PasswordGeneratorLibrary
+{
+    public interface IPasswordGenerator
+    {
+        string GenerateSalt(int saltSize = 8);
+        string GenerateHash(string plainText, string salt);
+    }
+
+    public class PasswordGenerator : IPasswordGenerator
+    {
+        public string GenerateSalt(int saltSize = 8)
+        {
+            var saltBytes = new byte[saltSize];
+            using (var rng = new RNGCryptoServiceProvider())
+                rng.GetNonZeroBytes(saltBytes);
+
+            return Convert.ToBase64String(saltBytes);
+        }
+                
+        public string GenerateHash(string plainText, string salt)
+        {
+            var saltBytes = Convert.FromBase64String(salt);
+            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            var plainTextWithSaltBytes = plainTextBytes.Concat(saltBytes).ToArray();
+
+            using (var hasher = new SHA512Managed())
+            {
+                var hashBytes = hasher.ComputeHash(plainTextWithSaltBytes);
+                var hashWithSaltBytes = hashBytes.Concat(saltBytes).ToArray();
+                return Convert.ToBase64String(hashWithSaltBytes);
+            }
+        }
+    }
+}
---
+++ PasswordGeneratorLibrary/PasswordGeneratorLibrary.Tests/PasswordGeneratorTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace PasswordGeneratorLibrary.Tests
+{
+    public class PasswordGeneratorTests
+    {
+        [Fact]
+        public void CanHashPassword()
+        {
+            var g = new PasswordGenerator();
+            var salt = g.GenerateSalt();
+            salt.Length.Should().BeGreaterOrEqualTo(4);
+        }
+
+        [Fact]
+        public void CanGeneratePassword()
+        {
+            var g = new PasswordGenerator();
+            var salt = g.GenerateSalt(128);
+            var hash = g.GenerateHash("TESTPASSWORDMETWATEXTRATEXT", salt);
+            hash.Length.Should().BeGreaterOrEqualTo(4);
+
+            var hash2 = g.GenerateHash("TESTPASSWORDMETWATEXTRATEXT", salt);
+            hash.Should().Be(hash2);
+        }
+    }
+}
